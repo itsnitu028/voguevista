a52ac98ff8bad0d78f7707d46d7025257d892729
unknown <itsnitu028@gmail.com>
2024-06-08 06:20:59 +0530

    added logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
       </div>
   );
 }
 
 export default App;
+
+//18:40
