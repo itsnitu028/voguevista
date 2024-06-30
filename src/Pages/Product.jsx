4645abd4916944095b6800582660a6f521aa527c
unknown <itsnitu028@gmail.com>
2024-07-01 01:05:09 +0530

    before description

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import {ShopContext} from '../Context/ShopContext'
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 function Product() {
+  const {all_product}=useContext(ShopContext);
+  const {productId}=useParams();
+  const product=all_product.find((e)=>e.id===Number(productId));
+  console.log(product);
   return (
     <div>
-     
+     <Breadcrum product={product}/>
+     <ProductDisplay product={product} />
       </div>
   );
 }
