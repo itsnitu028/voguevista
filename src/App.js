294f14f9f4ebf5b01cc709ef840f1800d8c84a7a
unknown <itsnitu028@gmail.com>
2024-06-30 06:35:17 +0530

    before login

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import LoginSignup from './Pages/LoginSignup';
 import Cart from './Pages/Cart';
 import ShopCategory from './Pages/ShopCategory'
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
 
 function App() {
   return (
@@ -16,9 +19,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Shop />} />
-        <Route path='/mens' element={<ShopCategory category="men"/>} />
-        <Route path='/womens' element={<ShopCategory category="women"/>} />
-        <Route path='/kids' element={<ShopCategory category="kid"/>} />
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>} />
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>} />
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>} />
         <Route path='/product' element={<Product />} >
          <Route path=':productId' element={<Product />} />
         </Route>
@@ -35,3 +38,4 @@ export default App;
 
 //54.17
 //1.05.16
+//1.39.00
