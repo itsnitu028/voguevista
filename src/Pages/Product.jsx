6be08b58c19e3f2be03044217fcce4b03b2304fc
unknown <itsnitu028@gmail.com>
2024-07-02 02:39:53 +0530

    before responsive complete react

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,6 +3,8 @@ import {ShopContext} from '../Context/ShopContext'
 import { useParams } from "react-router-dom";
 import Breadcrum from "../Components/Breadcrums/Breadcrum";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../Components/Description/DescriptionBox";
+import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 function Product() {
   const {all_product}=useContext(ShopContext);
   const {productId}=useParams();
@@ -12,6 +14,8 @@ function Product() {
     <div>
      <Breadcrum product={product}/>
      <ProductDisplay product={product} />
+     <DescriptionBox />
+     <RelatedProducts />
       </div>
   );
 }
